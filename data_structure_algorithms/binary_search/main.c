821e31064e615110097c65d91dee20b1d2cbaad8
Adika Bintang Sulaeman <adika.bintang@ui.ac.id>
2018-04-14 22:31:31 +0700

    add macro for status

--- data_structure_algorithms/binary_search/main.c
+++ data_structure_algorithms/binary_search/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #define ARRAY_SIZE 10
+#define BS_SEARCH_NOT_FOUND -1
 
 int bs_search(int *array, int searched_data, unsigned int array_size) {
     unsigned int low = 0;
@@ -21,7 +22,7 @@ int bs_search(int *array, int searched_data, unsigned int array_size) {
         }
     }
 
-    return -1;
+    return BS_SEARCH_NOT_FOUND;
 }
 
 int main()
@@ -29,7 +30,7 @@ int main()
     int my_array[ARRAY_SIZE] = {10, 14, 19, 26, 27, 31, 33, 35, 42, 44};
     
     int index_search = bs_search(my_array, 19, ARRAY_SIZE);
-    if (index_search == -1) {
+    if (index_search == BS_SEARCH_NOT_FOUND) {
         printf("not found\n");
     }
     else {
